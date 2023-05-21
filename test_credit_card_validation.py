2113957530965a26a76481130b572e293a3cdb37
dsl2055 <job2019tmm@gmail.com>
2023-05-20 21:42:11 -0400

    added hackerrank test

--- test_credit_card_validation.py
+++ test_credit_card_validation.py
@@ -14,6 +14,15 @@ class CreditCardValidationTestCase(unittest.TestCase):
         self.assertEqual(validate_credit_card("44244x4424442444"), "Invalid")
         self.assertEqual(validate_credit_card("0525362587961578"), "Invalid")
         self.assertEqual(validate_credit_card("5123 - 3567 - 8912 - 3456"), "Invalid")
+        self.assertEqual(validate_credit_card("5123_3567_8912_3456"), "Invalid")
 
+    def test_hackerrank_testrun_credit_card_numbers(self):
+        self.assertEqual(validate_credit_card("6"),"Invalid")
+        self.assertEqual(validate_credit_card("4123456789123456"),"Valid")
+        self.assertEqual(validate_credit_card("5123-4567-8912-3456"),"Valid")
+        self.assertEqual(validate_credit_card("61234-567-8912-3456"),"Invalid")
+        self.assertEqual(validate_credit_card("4123356789123456"),"Valid")
+        self.assertEqual(validate_credit_card("5133-3367-8912-3456"),"Invalid")
+        self.assertEqual(validate_credit_card("5123 - 3567 - 8912 - 3456"),"Invalid")
 if __name__ == '__main__':
     unittest.main()
